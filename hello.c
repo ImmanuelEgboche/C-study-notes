d41989f3acd8302400756448a0f2ec59fe2d7042
ImmanuelEgboche <77640433+ImmanuelEgboche@users.noreply.github.com>
2022-06-25 02:14:09 +0100

    notes on C declaration and initalization

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h> // includes information on the standard library 
+
+int main() // this defines a function called main which has no arguements 
+{
+    // variable creation is done in two steps DECLARATION and INITIALIZATION
+    int x;        // DECLARATION
+    x = 123;      // INITIALIZATION
+    int y = 1234; // DECLARATION + INITIALIZATION
+
+    float gpa = 4.2;     // float
+    char name = 'C';     // single character must be single quotes
+    char test[] = "Oni"; // array of characters must be double quotes
+    
+    printf("This is %s\n", test);
+    /*
+    %c single character, %f for float, %d for number, %s and for string 
+    */
+    printf("This is %d", y);
+    return 0;
+}
